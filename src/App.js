402febe02424ce1256749e6435f7f8b5e8ca39b3
Knux2 <ben_ashfield@hotmail.com>
2019-05-29 13:47:15 +0100

    Basic setup for Card Container including CSS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header'
+import CardContainer from "./Components/CardContainer/CardContainer";
 
 function App() {
   return (
     <div>
       <Header/>
+      <CardContainer/>
     </div>
   );
 }
---
+++ src/Components/CardContainer/CardContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './CardContainer.css'
+
+class CardContainer extends React.Component {
+    render () {
+        return (
+            <section className="cardContainer">
+                <div>f</div>
+                <div>d</div>
+                <div>s</div>
+            </section>
+        )
+    }
+}
+
+export default CardContainer
