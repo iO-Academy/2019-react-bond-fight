10bfc16bd0af907c44591ebde6397090e1e6b1e7
Knux2 <ben_ashfield@hotmail.com>
2019-05-29 16:34:05 +0100

    added fetch request to get data and populate cards

--- src/Components/CardContainer/Card/Card.js
+++ src/Components/CardContainer/Card/Card.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './Card.css'
 
 class Card extends React.Component {
-
     constructor(props) {
         super(props)
         this.state = {
-        selectedFilms: this.props.selectedFilms
+            movie: this.props.selectedFilm
         }
+
     }
 
     render() {
 
         return(
             <article className="card">
-                <img src={this.state.selectedFilms.image}/>
-                <h2>{this.state.selectedFilms.name}</h2>
-                <h4 className="year">{this.state.selectedFilms.release_date}</h4>
+                <img src={this.state.movie.image}/>
+                <h2>{this.state.movie.name}</h2>
+                <h4 className="year">{this.state.movie.release_date}</h4>
             </article>
         )
     }
--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -1,16 +1,50 @@
 import React from 'react'
 import './CardContainer.css'
 import Card from "./Card/Card";
+import GifLoader from 'react-gif-loader';
 
 class CardContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+
+        this.getFilms()
+    }
+
+    getFilms = () => {
+        fetch('http://127.0.0.1:3001/Bond_Films')
+            .then(data => data.json())
+            .then(data => {
+                this.setState({films: data})
+            })
+    }
+
     render () {
-        return (
-            <section className="cardContainer">
-                <Card selectedFilms={{"_id":"5cee81d223702b8178b6741b","name":"Live and Let Die","release_date":"1973-07-05","image":"https://image.tmdb.org/t/p/w500/p34OScaro2KdISrT4EMfeh1aS0E.jpg"}}/>
-                <div>VS</div>
-                <Card selectedFilms={{"_id":"5cee81d223702b8178b6741e","name":"Goldfinger","release_date":"1964-09-17","image":"https://image.tmdb.org/t/p/w500/vBNbFU3OS6okJIQBOos1aZXpy2Z.jpg"}}/>
-            </section>
-        )
+        const overlayBlockStyle = {
+            height: "70vh"
+        }
+
+        if(this.state.films !== undefined) {
+            return (
+                <section className="cardContainer">
+                    <Card selectedFilm={this.state.films[0]}/>
+                    <div>VS</div>
+                    <Card selectedFilm={this.state.films[1]}/>
+                </section>
+            )
+        } else {
+            return (
+                <div className="error">
+                    <GifLoader
+                        loading={true}
+                        imageSrc="https://media.giphy.com/media/nbKKMfmeDknzq/giphy.gif"
+                        imageStyle={overlayBlockStyle}
+                        overlayBackground="rgba(0,0,0,0.5)"
+                    />
+                </div>
+            )
+        }
     }
 }
 
