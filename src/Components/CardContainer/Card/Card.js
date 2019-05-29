f1e7ded7880cc3e7307afea48c7a42fe407a8421
fredplatt <fred_platt@hotmail.com>
2019-05-29 13:28:01 +0100

    finished styling, removed test data

--- src/Components/CardContainer/Card/Card.js
+++ src/Components/CardContainer/Card/Card.js
@@ -14,12 +14,9 @@ class Card extends React.Component {
 
         return(
             <div className="card">
-                {/*<img src={this.state.selectedFilms.image}/>*/}
-                {/*<p>Name:{this.state.selectedFilms.name}</p>*/}
-                {/*<p>Original Release:{this.state.selectedFilms.release_date}</p>*/}
-                <img src="https://i.pinimg.com/236x/3f/0c/29/3f0c295559dea2a0ddca6c37798626c8--image-gif-nicolas-cage.jpg"/>
-                <p>Name:1 </p>
-                <p>Original Release:2</p>
+                <img src={this.state.selectedFilms.image}/>
+                <h2>{this.state.selectedFilms.name}</h2>
+                <h4>{this.state.selectedFilms.release_date}</h4>
             </div>
         )
     }
