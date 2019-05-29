a3ae63f50502bd9e53896412c395cbf017f4de85
HazDuck <peter.e.heyes@gmail.com>
2019-05-29 11:44:09 +0100

    created React component with dummy data

---
+++ src/Components/CardContainer/Card/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Card extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+        selectedFilms: this.props.selectedFilms
+        }
+    }
+
+    render() {
+
+        return(
+            <div>
+                {/*<img src={this.state.selectedFilms.image}/>*/}
+                {/*<p>Name:{this.state.selectedFilms.name}</p>*/}
+                {/*<p>Original Release:{this.state.selectedFilms.release_date}</p>*/}
+                <img src="https://i.pinimg.com/236x/3f/0c/29/3f0c295559dea2a0ddca6c37798626c8--image-gif-nicolas-cage.jpg"/>
+                <p>Name:1 </p>
+                <p>Original Release:2</p>
+            </div>
+        )
+    }
+}
+
+export default Card
